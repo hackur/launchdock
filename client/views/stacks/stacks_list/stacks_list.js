a51433b89c6853d552bca22fd4c40db3fb23654c
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-07-31 17:51:25 -0400

    add publicUrl to Stacks schema

--- client/views/stacks/stacks_list/stacks_list.js
+++ client/views/stacks/stacks_list/stacks_list.js
@@ -14,11 +14,11 @@ Template.stacks_list.helpers({
       rowsPerPage: 20,
       showFilter: true,
       fields: [
-        { key: 'uuid', label: 'Tutum UUID', sortable: false },
+        { key: 'publicUrl', label: 'URL', tmpl: Template.stacks_list_url, sortable: false},
         { key: 'name', label: 'Name'},
         { key: 'state', label: 'State'},
-        { key: 'createdAt', label: 'Created', fn: function (val) {
-          return moment(val).format('LLL') + " (" + moment(val).fromNow() +")" } },
+        { key: 'createdAt', label: 'Created', sort: 'descending',
+          fn: function (val) { return moment(val).format('LLL') + " (" + moment(val).fromNow() +")" } },
         { key: 'actions', label: 'Actions', tmpl: Template.stacks_list_actions, sortable: false }
       ]
     };
