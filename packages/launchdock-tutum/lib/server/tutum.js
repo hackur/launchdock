af2f3bd272fedc541c4eee299cb435f3e852f420
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-08-20 11:12:14 -0400

    more tutum helper methods for stack create

--- packages/launchdock-tutum/lib/server/tutum.js
+++ packages/launchdock-tutum/lib/server/tutum.js
@@ -7,7 +7,7 @@ Tutum = function Tutum (username, token) {
 
   this.checkCredentials = function () {
     if (!this.username || !this.token) {
-      throw new Meteor.Error(407, "Missing Tutum API credentials.");
+      throw new Meteor.Error("Missing Tutum API credentials.");
     }
   };
 }
@@ -134,3 +134,72 @@ Tutum.prototype.addLinkToLoadBalancer = function (linkedServiceName, linkedServi
     }
   });
 };
+
+
+Tutum.prototype.updateStackServices = function (services) {
+  check(services, [String]);
+
+  var self = this;
+
+  _.each(services, function (service_uri) {
+    try {
+      var service = self.get(service_uri);
+    } catch(e) {
+      return e;
+    }
+    Services.update({ uri: service_uri }, {
+      $set: {
+        name: service.data.name,
+        uuid: service.data.uuid,
+        imageName: service.data.image_name,
+        stack: service.data.stack,
+        state: service.data.state,
+        tags: service.data.tags,
+        uri: service_uri
+      }
+    }, {
+      upsert: true
+    });
+  });
+}
+
+
+Tutum.prototype.getServiceContainers = function (serviceUri) {
+  check(serviceUri, String);
+
+  try {
+    var service = this.get(serviceUri);
+    return service.data.containers;
+  } catch(e) {
+    return e;
+  }
+}
+
+
+Tutum.prototype.checkMongoState = function (containerUuid, callback) {
+  check(containerUuid, String);
+  check(callback, Match.Optional(Function));
+
+  var command = 'mongo --nodb /opt/mongo/upcheck.js';
+  var url = 'wss://stream.tutum.co/v1/container/' + containerUuid + '/exec/?user=' + this.username + '&token=' + this.token;
+      url += '&command=' + command;
+
+  var WebSocket = Npm.require('ws');
+  var socket = new WebSocket(url);
+
+  socket.on('open', function() {
+    console.log('Tutum shell websocket opened');
+  });
+
+  socket.on('message', function(messageStr) {
+    var msg = JSON.parse(messageStr);
+    // console.log(msg.output);
+    if (msg.output === 'Mongo primary is ready for connections') {
+      callback(null, true);
+    }
+  });
+
+  socket.on('close', function() {
+    console.log('Tutum shell websocket closed');
+  });
+}
