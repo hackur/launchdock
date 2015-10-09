d1ee23138aace0aa561a7d71c9f6d8fc7f070223
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-10-08 23:50:16 -0400

    back to bunyan

--- packages/launchdock-lib/lib/server/logger.js
+++ packages/launchdock-lib/lib/server/logger.js
@@ -2,26 +2,45 @@
  * Global logging config
  */
 
-var winston = Npm.require('winston');
-Npm.require('winston-loggly');
+// Using ongoworks:bunyan-logger
+// https://atmospherejs.com/ongoworks/bunyan-logger
 
-Logger = new winston.Logger();
+// https://www.npmjs.com/package/bunyan-loggly
+var Bunyan2Loggly = Npm.require('bunyan-loggly').Bunyan2Loggly;
 
-// default console transport
-Logger.add(winston.transports.Console, {
-  level: process.env.LAUNCHDOCK_LOG_LEVEL || 'info',
-  timestamp: true,
-  colorize: true,
-  prettyPrint: true
-});
+var logLevel = process.env.LAUNCHDOCK_LOG_LEVEL || "INFO";
 
-// only include Loggly transport in production
-if (process.env.NODE_ENV === 'production') {
-  Logger.add(winston.transports.Loggly, {
-    token: process.env.LOGGLY_TOKEN,
-    subdomain: process.env.LOGGLY_SUBDOMAIN,
-    level: 'info',
-    json: true
+if (process.env.VELOCITY_CI === "1") {
+  formatOut = process.stdout;
+} else {
+  formatOut = logger.format({
+    levelInString: false
   });
 }
 
+// default console config
+var streams = [{
+  level: 'info',
+  stream: logLevel !== "DEBUG" ? formatOut : process.stdout,
+}]
+
+// Loggly config (only used in production)
+var logglyStream = {
+  type: 'raw',
+  stream: new Bunyan2Loggly({
+    token: process.env.LOGGLY_TOKEN,
+    subdomain: process.env.LOGGLY_SUBDOMAIN,
+  })
+}
+
+// if (process.env.NODE_ENV === "production") {
+  streams.push(logglyStream);
+// }
+
+// create default logger instance
+Logger = logger.bunyan.createLogger({
+  name: "Launchdock",
+  streams: streams
+});
+
+Logger.level(logLevel);
