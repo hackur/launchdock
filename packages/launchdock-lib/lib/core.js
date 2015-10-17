95acce7e2fa948e39cb7c88871bf883f007f034d
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-10-17 14:55:24 -0400

    add isProduction helper

--- packages/launchdock-lib/lib/core.js
+++ packages/launchdock-lib/lib/core.js
@@ -2,7 +2,15 @@
  * Kick off the global namespace for Launchdock.
  * @namespace Launchdock
  */
-
 Launchdock = {};
 
 Launchdock.VERSION = '0.1.0';
+
+
+/**
+ * Check if we're running in production
+ * @return {Boolean} true if NODE_ENV is 'production'
+ */
+Launchdock.isProduction = function () {
+  return (process.env.NODE_ENV === 'production');
+}
