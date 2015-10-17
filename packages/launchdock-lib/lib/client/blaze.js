888ab5fe1f27e273d58c10bb66e87cbce488f817
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-10-17 13:48:15 -0400

    more template helpers

--- packages/launchdock-lib/lib/client/blaze.js
+++ packages/launchdock-lib/lib/client/blaze.js
@@ -1,8 +1,30 @@
-
-Template.registerHelper('moment', function (date, format) {
+// date helpers
+Template.registerHelper('moment', (date, format) => {
   return moment(date).format(format);
-})
+});
 
-Template.registerHelper('timeAgo', function (date) {
+Template.registerHelper('timeAgo', (date) => {
   return moment(date).fromNow();
-})
+});
+
+
+// user helpers
+Template.registerHelper('isAdmin', (id) => {
+  return Roles.userIsInRole(id, 'admin');
+});
+
+Template.registerHelper('isCurrentUser', (currentUser) => {
+  return (currentUser === Meteor.userId());
+});
+
+Template.registerHelper('disableIfAdmin', (userId) => {
+  if (Meteor.userId() === userId) {
+    return Roles.userIsInRole(userId, 'admin') ? "disabled" : "";
+  }
+});
+
+
+// misc
+Template.registerHelper('isEqual', (val1, val2) => {
+  return (val1 === val2);
+});
