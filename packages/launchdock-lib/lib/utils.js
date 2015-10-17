e17bd46366bfa152fd5d6573ba3e1951b032774c
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-10-17 14:53:06 -0400

    comments

--- packages/launchdock-lib/lib/utils.js
+++ packages/launchdock-lib/lib/utils.js
@@ -2,13 +2,13 @@
 /**
  *  Launchdock Utils
  */
-
 Launchdock.utils = {};
 
 
 /**
  * Slugify a string
- * @param {String}
+ * @param {String} str - a string to be converted to a url-friendly slug
+ * @return {String} slugified string
  */
 Launchdock.utils.slugify = function (str) {
   // underscorestring:underscore.string
