71607bedd265fe39a6b7f7333ef2ae5794871793
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-11-12 13:58:42 -0500

    add option to add Drive url as env var

--- packages/launchdock-accounts-reaction/lib/server/main.js
+++ packages/launchdock-accounts-reaction/lib/server/main.js
@@ -5,12 +5,18 @@ Logger = Logger.child({ meteor_package: 'launchdock:accounts-reaction' });
  * Reaction Drive config
  */
 Launchdock.config.drive = {
+
   url() {
-    const settings = Meteor.settings.public.drive;
-    if (settings && settings.url) {
-      return settings.url
+    const urlEnvVar = process.env.REACTION_DRIVE_URL;
+    const settings = Meteor.settings.drive;
+
+    if (urlEnvVar) {
+      return urlEnvVar;
+    } else if (settings && settings.url) {
+      return settings.url;
     } else {
-      return "https://reactioncommerce.com/"
+      return "https://reactioncommerce.com/";
     }
   }
+
 }
