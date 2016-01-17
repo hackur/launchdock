3da61fc8d30065d013874bf06eeabbff236b408c
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-01-17 14:45:15 -0600

    update LD user after Stripe customer creation, add update method

--- packages/launchdock-stripe/lib/server/methods.js
+++ packages/launchdock-stripe/lib/server/methods.js
@@ -64,25 +64,109 @@ Meteor.methods({
         }
       });
     } catch(e) {
-      const err = "Error creating new Stripe customer.";
-      logger.error(err, e);
-      throw new Meteor.Error(err);
+      logger.error(e);
+      throw new Meteor.Error(e);
     }
 
     logger.info("Successfully created and subscribed new customer.", customer);
 
+    const subscription = customer.subscriptions.data[0];
+
+    // remove unwanted fields from plan object
+    delete subscription.plan.object;
+    delete subscription.plan.livemode;
+    delete subscription.plan.metadata;
+    delete subscription.plan.statement_descriptor;
+    delete subscription.plan.trial_period_days;
+
     Users.update({ _id: user._id }, {
       $set: {
         "stripe.customerId": customer.id,
-        "stripe.plan": options.plan,
-        "stripe.created": customer.created,
-        "stripe.status": "subscribed"
+        "subscription.created": new Date(customer.created * 1000),
+        "subscription.plan": subscription.plan,
+        "subscription.status": subscription.status,
+        "subscription.next_payment": new Date(subscription.current_period_end * 1000)
       }
     }, (err, num) => {
       if (err) {
         logger.error(err);
       } else {
-        logger.debug("User updated with Stripe customer details.");
+        logger.info("User updated with new Stripe customer details.");
+      }
+    });
+
+    return customer;
+  },
+
+
+  /**
+   * Update data for a Stripe customer
+   * @param  {String} userId - user to update
+   * @return {Object} stripe customer object
+   */
+  'stripe/updateCustomer'(userId) {
+
+    const logger = Logger.child({
+      meteor_method: "stripe/updateCustomer",
+      meteor_method_args: userId,
+      userId: this.userId
+    });
+
+    if (!this.userId) {
+      const err = "AUTH ERROR: Must be logged in.";
+      logger.error(err);
+      throw new Meteor.Error(err);
+    }
+
+    check(userId, String);
+
+    this.unblock();
+
+    // if trying to update a user other than the current user,
+    // make sure the user is an admin
+    if (userId !== this.userId && !Users.is.admin(this.userId)) {
+      const err = "AUTH ERROR: Insufficient permissions.";
+      logger.error(err);
+      throw new Meteor.Error(err);
+    }
+
+    const user = Users.findOne({ _id: userId });
+
+    // make sure the specified user already has a Stripe customer created
+    if (!user.stripe && !user.stripe.customerId) {
+      const err = "No Stripe customer ID found for user.";
+      logger.error(err);
+      throw new Meteor.Error(err);
+    }
+
+    const key = Launchdock.stripe.getKey("secret");
+    const stripe = StripeSync(key);
+
+    // retrive customer from Stripe
+    let customer;
+    try {
+      customer = stripe.customers.retrieve(user.stripe.customerId);
+    } catch(e) {
+      logger.error(e);
+      throw new Meteor.Error(e);
+    }
+
+    // grab the subscription details
+    const subscription = customer.subscriptions.data[0];
+
+    // remove unwanted fields from plan object
+    delete subscription.plan.object;
+    delete subscription.plan.livemode;
+    delete subscription.plan.metadata;
+    delete subscription.plan.statement_descriptor;
+    delete subscription.plan.trial_period_days;
+
+    // update the subscription details for the Launchdock user
+    Users.update({ _id: user._id }, {
+      $set: {
+        "subscription.plan": subscription.plan,
+        "subscription.status": subscription.status,
+        "subscription.next_payment": new Date(subscription.current_period_end * 1000)
       }
     });
 
