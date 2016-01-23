c769d9f3c2fa9cb989e4f94b2ec32b7cb5f0be82
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-01-22 20:54:12 -0500

    cleanup

--- client/views/stacks/stacks_list/stacks_list_actions.js
+++ client/views/stacks/stacks_list/stacks_list_actions.js
@@ -1,15 +1,15 @@
 
 Template.stacks_list_actions.events({
-  'click .delete-stack' (e, t) {
+  "click .delete-stack"() {
     Alert.confirm({
       title: "Are you sure?",
       text: "There's no going back!"
     }, () => {
-      Meteor.call('tutum/deleteStack', this._id, (err, res) => {
+      Meteor.call("tutum/deleteStack", this._id, (err) => {
         if (err) {
           Alert.error({
             title: "Oops!",
-            text: `Something went wrong deleting the stack. <br> ${err}`
+            text: `Something went wrong deleting the stack.`
           });
         }
       });
