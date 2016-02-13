3893c244a98102251d466921d5c3abe7240fbd35
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-02-13 14:39:39 -0500

    create ridiculously complex method for deleting SSL certs

--- packages/launchdock-rancher/lib/server/methods/certs.js
+++ packages/launchdock-rancher/lib/server/methods/certs.js
@@ -120,6 +120,129 @@ Meteor.methods({
     });
 
     return cert;
+  },
+
+
+  'rancher/deleteStackSSLCert'(stackId) {
+
+    const logger = Logger.child({
+      meteor_method: 'rancher/deleteStackSSLCert',
+      meteor_method_args: stackId,
+      userId: this.userId
+    });
+
+    check(stackId, String);
+
+    const stack = Stacks.findOne(stackId);
+
+    if (!stack) {
+      const err = "Stack not found";
+      logger.error(err);
+      throw new Meteor.Error(err);
+    }
+
+    if (!stack.sslRancherCertId) {
+      const msg = "Stack does not have a custom certificate. Skipping deletion.";
+      logger.info(msg, stack);
+      return msg;
+    }
+
+    if (!Users.is.owner(this.userId, stack) && !Users.is.admin(this.userId)) {
+      const err = "AUTH ERROR: Access denied";
+      logger.error(err);
+      throw new Meteor.Error(err);
+    }
+
+    const rancher = new Rancher();
+    rancher.checkCredentials();
+
+    const balancerId = Settings.get("rancherDefaultBalancer");
+
+    // get the load balancer for this stack
+    let lb;
+    try {
+      lb = rancher.get("loadbalancerservices", balancerId);
+    } catch(e) {
+      const err = "Error getting load balancer details from Rancher.";
+      logger.error(err, e);
+      throw new Meteor.Error(err);
+    }
+
+    // cert to be deleted
+    const certId = stack.sslRancherCertId;
+
+    // certs currently on load balancer
+    let lbCerts = lb.data.certificateIds || [];
+
+    // check if certificate is in use on the load balancer
+    if (lbCerts.find(certId => certId === certId)) {
+      // remove certificate from the array
+      lbCertsUpdated = _.without(lbCerts, certId);
+
+      // update load balancer with remaining certs
+      const lbUpdateOptions = { certificateIds: lbCertsUpdated };
+      try {
+        rancher.update("loadbalancerservices", balancerId, lbUpdateOptions);
+        logger.info(`Certificate ${certId} removed from load balancer ${balancerId}.`);
+      } catch(e) {
+        const err = "Error updating load balancer certificates.";
+        logger.error(err, e);
+        throw new Meteor.Error(err);
+      }
+
+      // get the app service
+      const app = Services.findOne({ name: `app-${stackId}` });
+
+      // remove the balancer => app links
+      // (Rancher only lets us remove ALL links, so we add the wildcard back below)
+      try {
+        rancher.removeLoadBalancerLink(balancerId, app.rancherId);
+        logger.info(`Balancer links removed for app ${app.rancherId}.`);
+      } catch(e) {
+        const err = "Error removing load balancer links.";
+        logger.error(err, e);
+        throw new Meteor.Error(err);
+      }
+
+      // add back the balancer => app link for the wildcard domain
+      try {
+        rancher.addLoadBalancerLink(balancerId, app.rancherId, stack.defaultDomain);
+        logger.info(`Link added to load balancer ${balancerId} for app ${app.rancherId}.`);
+      } catch(e) {
+        const err = "Error updating load balancer links.";
+        logger.error(err, e);
+        throw new Meteor.Error(err);
+      }
+    } else {
+      logger.info("Certificate not found on load balancer. No update needed.");
+    }
+
+    // delete the certificate
+    try {
+      rancher.delete("certificates", certId);
+      logger.info(`Certificate ${certId} deleted.`);
+    } catch(e) {
+      const err = "Error deleting certificate.";
+      logger.error(err, e);
+      throw new Meteor.Error(err);
+    }
+
+    // remove certificate details from the local stack
+    Stacks.update({ _id: stackId }, {
+      $unset: {
+        sslDomainName: "",
+        sslPrivateKey: "",
+        sslPublicCert: "",
+        sslCertChain: "",
+        sslPem: "",
+        sslCertDescription: "",
+        sslRancherCertId: ""
+      }
+    });
+
+    logger.info(`Certificate ${certId} successfully removed from stack ${stackId}`);
+
+    return true;
   }
-  
+
 });
