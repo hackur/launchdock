a95b72fb9c745bb934446cafc90bf58e46029752
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-02-15 09:11:18 -0500

    add ALT + click to delete stacks without confirmation

--- client/views/stacks/stacks_list/stacks_list_actions.js
+++ client/views/stacks/stacks_list/stacks_list_actions.js
@@ -1,21 +1,35 @@
 
 Template.stacks_list_actions.events({
-  "click .delete-stack"() {
+  "click .delete-stack"(e, t) {
     const stack = Template.instance().data;
     const platform = stack.platform.toLowerCase();
 
-    Alert.confirm({
-      title: "Are you sure?",
-      text: "There's no going back!"
-    }, () => {
+    // hard delete if ALT+click, else confirm prompt
+    if (e.altKey) {
       Meteor.call(`${platform}/deleteStack`, this._id, (err) => {
         if (err) {
           Alert.error({
             title: "Oops!",
             text: `Something went wrong deleting the stack.`
           });
+          return;
         }
+        Notify.success("Successfully deleted!", "top-right");
       });
-    });
+    } else {
+      Alert.confirm({
+        title: "Are you sure?",
+        text: "There's no going back!"
+      }, () => {
+        Meteor.call(`${platform}/deleteStack`, this._id, (err) => {
+          if (err) {
+            Alert.error({
+              title: "Oops!",
+              text: `Something went wrong deleting the stack.`
+            });
+          }
+        });
+      });
+    }
   }
 });
