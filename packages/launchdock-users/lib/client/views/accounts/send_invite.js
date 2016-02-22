9957e311b36830b412dba0c85eb4b884b6248df1
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-02-22 15:00:17 -0500

    linting

--- packages/launchdock-users/lib/client/views/accounts/send_invite.js
+++ packages/launchdock-users/lib/client/views/accounts/send_invite.js
@@ -1,37 +1,37 @@
 
 Template.send_invite.events({
-  'click button[type="submit"]' (e, t) {
+  "click button.invite-user"(e, t) {
     e.preventDefault();
 
-    const isManager = Roles.userIsInRole(Meteor.userId(), 'manager');
+    const isManager = Roles.userIsInRole(Meteor.userId(), "manager");
 
     const options = {
       email: t.find("[name='invite-user-email']").value,
-      role: isManager ? 'customer' : t.find("[name='invite-user-role'] option:selected").value
-    }
+      role: isManager ? "customer" : t.find("[name='invite-user-role'] option:selected").value
+    };
 
     if (options.email && options.role) {
-      if (options.role === 'customer') {
-        Meteor.call('sendReactionInvite', options, (err, res) => {
+      if (options.role === "customer") {
+        Meteor.call("sendReactionInvite", options, (err) => {
           if (err) {
             Notify.error(err.error);
           } else {
-            $('#invite-user-modal').modal('hide');
-            Notify.success('Invitation sent!');
+            $("#invite-user-modal").modal("hide");
+            Notify.success("Invitation sent!");
           }
         });
       } else {
-        Meteor.call('sendUserInvite', options, (err, res) => {
+        Meteor.call("sendUserInvite", options, (err) => {
           if (err) {
             Notify.error(err.error);
           } else {
-            $('#invite-user-modal').modal('hide');
-            Notify.success('Invitation sent!');
+            $("#invite-user-modal").modal("hide");
+            Notify.success("Invitation sent!");
           }
         });
       }
     } else {
-      Notify.warn('Please set an email and at least one role!');
+      Notify.warn("Please set an email and at least one role!");
     }
   }
 });
