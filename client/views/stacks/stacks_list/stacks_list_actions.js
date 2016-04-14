7fac0f11130f03a835e193fb9abfad81eaa84d94
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-04-14 10:34:09 -0400

    remove platform check from stack delete, linting

--- client/views/stacks/stacks_list/stacks_list_actions.js
+++ client/views/stacks/stacks_list/stacks_list_actions.js
@@ -1,32 +1,31 @@
 import { Notify, Alert } from '/client/modules/core/configs/notifications';
 
 Template.stacks_list_actions.events({
-  "click .delete-stack"(e, t) {
+  'click .delete-stack'(e, t) {
     const stack = Template.instance().data;
-    const platform = stack.platform.toLowerCase();
 
     // hard delete if ALT+click, else confirm prompt
     if (e.altKey) {
-      Meteor.call(`${platform}/deleteStack`, this._id, (err) => {
+      Meteor.call('rancher/deleteStack', this._id, (err) => {
         if (err) {
           Alert.error({
-            title: "Oops!",
-            text: "Something went wrong deleting the stack."
+            title: 'Oops!',
+            text: 'Something went wrong deleting the stack.'
           });
           return;
         }
-        Notify.success("Successfully deleted!", "top-right");
+        Notify.success('Successfully deleted!', 'top-right');
       });
     } else {
       Alert.confirm({
-        title: "Are you sure?",
-        text: "There's no going back!"
+        title: 'Are you sure?',
+        text: 'There\'s no going back!'
       }, () => {
-        Meteor.call(`${platform}/deleteStack`, this._id, (err) => {
+        Meteor.call('rancher/deleteStack', this._id, (err) => {
           if (err) {
             Alert.error({
-              title: "Oops!",
-              text: "Something went wrong deleting the stack."
+              title: 'Oops!',
+              text: 'Something went wrong deleting the stack.'
             });
           }
         });
