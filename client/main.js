110b62da7f978826f104436f951c2f3f56e5f7fe
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-09-30 14:50:38 -0400

    load jobs module on client

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import initContext from './configs/context';
 
 // modules
 import coreModule from './modules/core';
+import jobsModule from './modules/jobs';
 import settingsModule from './modules/settings';
 import stacksModule from './modules/stacks';
 import usersModule from './modules/users';
@@ -13,6 +14,7 @@ const context = initContext();
 // create app
 const app = createApp(context);
 app.loadModule(coreModule);
+app.loadModule(jobsModule);
 app.loadModule(settingsModule);
 app.loadModule(stacksModule);
 app.loadModule(usersModule);
