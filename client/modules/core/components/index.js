6d76031b4e36bdf60327ecde2476242c05f35eb1
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-10-13 23:57:45 -0400

    add FieldGroup component and UI index file

---
+++ client/modules/core/components/index.js
@@ -0,0 +1,15 @@
+import Dashboard from './dashboard';
+import error from './error';
+import FieldGroup from './ui/field_group';
+import Loading from './loading';
+import NotFound from './not_found';
+import SideNav from './side_nav';
+
+export {
+  Dashboard,
+  error,
+  FieldGroup,
+  Loading,
+  NotFound,
+  SideNav
+};
